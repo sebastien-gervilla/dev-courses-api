0233aa1937c51dfe94113ef0ace8eda8fea32118
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-05-28 10:50:55 +0200

    Feat: TutorialById route

--- src/routes/tutorial.routes.ts
+++ src/routes/tutorial.routes.ts
@@ -3,7 +3,7 @@ import express from 'express';
 
 // Controller imports
 import { 
-    getTutorials, getTutorial,
+    getTutorials, getTutorial, getTutorialById,
     createTutorial, followTutorial,
     updateTutorial,
     deleteTutorial
@@ -20,5 +20,7 @@ Router.get('/:slug', isAuth, getTutorial);
 Router.post('/:id/follow', isAuth, followTutorial)
 Router.put('/:id', isAuth, isAdmin, updateTutorial);
 Router.delete('/:id', isAuth, isAdmin, deleteTutorial);
+
+Router.get('/editor/:id', isAuth, isAdmin, getTutorialById)
 
 export default Router;
